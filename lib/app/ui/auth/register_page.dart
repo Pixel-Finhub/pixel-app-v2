7fe4fb7cb309194c21a90d2df93af02fca5c5b96
JumaRubea <rubeajuma8@gmail.com>
2023-08-30 22:49:40 +0300

    chore: claiming documentation one done

--- lib/app/ui/auth/register_page.dart
+++ lib/app/ui/auth/register_page.dart
@@ -23,127 +23,125 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
-      body: SafeArea(
-        minimum: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Form(
-                key: formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      //minimum: const EdgeInsets.all(20.0),
+      //safeArea is removed to enble keyboard space
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 70.0.h,
+                  ),
+                  Center(
+                    child: Image.asset(
+                      'assets/images/register.png',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0.h,
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      side: const BorderSide(
+                        color: primary,
+                        width: 1.0,
+                      ),
+                    ),
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                        vertical: 3.0,
+                      ),
+                      child: TextFormField(
+                        controller: nidaTextEditController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: "NIDA Number",
+                          border: InputBorder.none,
+                        ),
+                        // onChanged: (value) => controller.nidaNumber(value),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      side: const BorderSide(
+                        color: primary,
+                        width: 1.0,
+                      ),
+                    ),
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                        vertical: 3.0,
+                      ),
+                      child: InternationalPhoneNumberInput(
+                        onInputChanged: (PhoneNumber number) {
+                          // controller.phoneNumber(number.phoneNumber);
+                        },
+                        onInputValidated: (bool value) {
+                          print(value);
+                          // controller.phoneNumber(number.phoneNumber);
+                        },
+                        selectorConfig: const SelectorConfig(
+                          selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                        ),
+                        ignoreBlank: false,
+                        autoFocus: false,
+                        autoValidateMode: AutovalidateMode.disabled,
+                        selectorTextStyle: const TextStyle(color: Colors.black),
+                        initialValue: number,
+                        textFieldController: phoneNumberTextEditController,
+                        formatInput: false,
+                        keyboardType: TextInputType.phone,
+                        inputBorder: InputBorder.none,
+                        onSaved: (PhoneNumber number) {
+                          print('On Saved: $number');
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30.0.h,
+            ),
+            CustomButton(
+              function: () {},
+              text: "Register",
+            ),
+            TextButton(
+              onPressed: () {
+                Get.to(() => const LoginPage());
+              },
+              child: RichText(
+                text: const TextSpan(
+                  text: "Already a user? ",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
                   children: [
-                    SizedBox(
-                      height: 70.0.h,
-                    ),
-                    Center(
-                      child: Image.asset(
-                        'assets/images/register.png',
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30.0.h,
-                    ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(defaultRadius),
-                        side: const BorderSide(
-                          color: primary,
-                          width: 1.0,
-                        ),
-                      ),
-                      elevation: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10.0,
-                          vertical: 3.0,
-                        ),
-                        child: TextFormField(
-                          controller: nidaTextEditController,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            hintText: "NIDA Number",
-                            border: InputBorder.none,
-                          ),
-                          // onChanged: (value) => controller.nidaNumber(value),
-                        ),
-                      ),
-                    ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(defaultRadius),
-                        side: const BorderSide(
-                          color: primary,
-                          width: 1.0,
-                        ),
-                      ),
-                      elevation: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10.0,
-                          vertical: 3.0,
-                        ),
-                        child: InternationalPhoneNumberInput(
-                          onInputChanged: (PhoneNumber number) {
-                            // controller.phoneNumber(number.phoneNumber);
-                          },
-                          onInputValidated: (bool value) {
-                            print(value);
-                            // controller.phoneNumber(number.phoneNumber);
-                          },
-                          selectorConfig: const SelectorConfig(
-                            selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
-                          ),
-                          ignoreBlank: false,
-                          autoFocus: false,
-                          autoValidateMode: AutovalidateMode.disabled,
-                          selectorTextStyle:
-                              const TextStyle(color: Colors.black),
-                          initialValue: number,
-                          textFieldController: phoneNumberTextEditController,
-                          formatInput: false,
-                          keyboardType: TextInputType.phone,
-                          inputBorder: InputBorder.none,
-                          onSaved: (PhoneNumber number) {
-                            print('On Saved: $number');
-                          },
-                        ),
+                    TextSpan(
+                      text: "Login",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: 30.0.h,
-              ),
-              CustomButton(
-                function: () {},
-                text: "Register",
-              ),
-              TextButton(
-                onPressed: () {
-                  Get.to(() => const LoginPage());
-                },
-                child: RichText(
-                  text: const TextSpan(
-                    text: "Already a user? ",
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: "Login",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:pixel_insurance_v2/app/claiming/claiming_process1.dart';
 import 'package:pixel_insurance_v2/app/ui/auth/splash.dart';
+
 void main() {
   runApp(
     ScreenUtilInit(
