543acfb4fcacfa106e59bd48f134c1b19d5771ca
KelvinHussle <kelvinkalinga99@gmail.com>
2023-08-30 11:35:56 +0400

    chore: setup homepage one

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,41 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.notifications, color: Colors.black, size: 30),
+        ),
+        actions: [
+          CircleAvatar(
+            child: Image.asset(
+              'assets/images/login_person.png',
+            ),
+          )
+          /* IconButton(
+            onPressed: () {},
+            icon: Icon(Icons., color: Colors.black, size: 30),
+          ),*/
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Welcome Mr. Msangi",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.grey,
+          ),
+          height: 300,
+        ),
+      ),
+    ));
   }
 }
