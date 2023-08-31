468d32b74658347498f632ae55f7d8aac219cf2c
eddygulled <edgargulay69@gmail.com>
2023-08-31 11:57:24 +0300

    login page buautified

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:pixel_insurance_v2/app/ui/auth/splash.dart';
+
 void main() {
   runApp(
     ScreenUtilInit(
