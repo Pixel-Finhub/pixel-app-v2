a6a559189f92617eeed3462a680f476b3ac0c421
PatrickMamsery <pkrobert1612@gmail.com>
2023-09-05 10:00:21 +0300

    fix: merge remote-tracking branch 'origin/develop' into develop and fix conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
-      home: PremiumPage(),
+      home: SplashScreen(),
     );
   }
 }
