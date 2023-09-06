19ed5b7dd3bc8aba7edaed262eb941af41c336c5
eddygulled <edgargulay69@gmail.com>
2023-09-06 13:09:48 +0300

    updated transported item grid with new asset images

--- lib/app/package/request_form.dart
+++ lib/app/package/request_form.dart
@@ -1,303 +1,182 @@
 import 'package:flutter/material.dart';
 import 'package:pixel_insurance_v2/app/ui/shared/custom_nav.dart';
+import 'package:pixel_insurance_v2/app/ui/theme/app_colors.dart';
+import 'package:pixel_insurance_v2/app/ui/utils/dimensions.dart';
 
 class RequestForm extends StatefulWidget {
-  const RequestForm({super.key});
+  const RequestForm({Key? key}) : super(key: key);
 
   @override
   State<RequestForm> createState() => _RequestFormState();
 }
 
 class _RequestFormState extends State<RequestForm> {
+  List<Item> items = [
+    Item(name: 'Fabrics', imagePath: 'assets/images/fabrics.png', borderColor: primary),
+    Item(name: 'Rice Packs', imagePath: 'assets/images/crops.png', borderColor: Colors.blue),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back),
-          color: Colors.black,
-        ),
-        backgroundColor: Colors.white,
-        title: const Text(
-          'Insurance Pre-Survey',
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        toolbarHeight: 70.0,
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications_active,
-                color: Colors.black,
-              ))
-        ],
-      ),
+      appBar: _buildAppBar(context),
       body: SingleChildScrollView(
         child: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
-                child: Text('Item on transit'),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 10),
-                    height: 108,
-                    width: 120,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all()),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 10),
-                          child: Text('Fabrics'),
-                        ),
-                        Image.asset('assets/images/fabrices.png')
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 10),
-                    height: 108,
-                    width: 120,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all()),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 10),
-                          child: Text('Fabrics'),
-                        ),
-                        Image.asset('assets/images/crops.png')
-                      ],
-                    ),
-                  ),
-                  Container(
-                      margin: const EdgeInsets.fromLTRB(3, 10, 0, 10),
-                      height: 108,
-                      width: 70,
-                      decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 191, 214, 164),
-                          borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              bottomLeft: Radius.circular(20)),
-                          border: Border.all()),
-                      child: const Icon(Icons.add))
-                ],
-              ),
-              const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                  child: Text('Transportation mode')),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                height: 36,
-                width: 340,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all()),
-                child: TextFormField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        label: Row(
-                          //mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            const SizedBox(
-                              width: 15,
-                            ),
-                            const Text('Truck'),
-                            const SizedBox(
-                              width: 125,
-                            ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.add,
-                              ),
-                            ),
-                            const Text('Truck details')
-                          ],
-                        ))),
-              ),
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(5, 10, 0, 5),
-                        child: Text('Duration'),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 15),
-                        height: 35,
-                        width: 72,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all()),
-                        child: TextFormField(
-                            decoration: const InputDecoration(
-                                border: InputBorder.none,
-                                label: Row(
-                                  //mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-                                    Text('5 days'),
-                                  ],
-                                ))),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                        child: Text('Expected shipment Date'),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 25),
-                        height: 35,
-                        width: 230,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all()),
-                        child: TextFormField(
-                            decoration: const InputDecoration(
-                                border: InputBorder.none,
-                                label: Row(
-                                  //mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-                                    Text('25-Jan-2024'),
-                                  ],
-                                ))),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(5, 10, 0, 5),
-                        child: Text('Origin'),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 15),
-                        height: 35,
-                        width: 154,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all()),
-                        child: TextFormField(
-                            decoration: const InputDecoration(
-                                border: InputBorder.none,
-                                label: Row(
-                                  //mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-                                    Text('Iringa'),
-                                  ],
-                                ))),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                        child: Text('Destination'),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 35,
-                        width: 154,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all()),
-                        child: TextFormField(
-                            decoration: const InputDecoration(
-                                border: InputBorder.none,
-                                label: Row(
-                                  //mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-                                    Text('Dar es Salaam'),
-                                  ],
-                                ))),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(15, 15, 0, 5),
-                child: Text('Value of the Cargo'),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 15),
-                height: 36,
-                width: 340,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all()),
-                child: TextFormField(
-                    decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        label: Row(
-                          //mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Text('Cargo Price:'),
-                            SizedBox(
-                              width: 125,
-                            ),
-                            Text('50000 Tzs')
-                          ],
-                        ))),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Center(
-                child: SizedBox(
-                  height: 40,
-                  width: 230,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50))),
-                    child: const Text('Continue'),
-                  ),
-                ),
-              ),
-            ]),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildSectionTitle(context, 'Item to transport'),
+            _buildItemGrid(context),
+            const SizedBox(height: 50),
+            _buildSendRequestButton(context),
+          ],
+        ),
       ),
-      bottomNavigationBar: CustomNav(currentIndex: 3),
+      bottomNavigationBar: const CustomNav(currentIndex: 3),
     );
   }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.arrow_back),
+        color: Colors.black,
+      ),
+      backgroundColor: Colors.white,
+      title: Text(
+        'Insurance Pre-Survey',
+        style: TextStyle(
+          fontWeight: FontWeight.w400,
+          color: primaryDark,
+          fontSize: fluidFontSize(context, 18),
+        ),
+      ),
+      centerTitle: true,
+      toolbarHeight: 70.0,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.notifications_active,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: fluidWidth(context, 5)),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: fluidFontSize(context, 15),
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItemGrid(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: fluidWidth(context, 5)),
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _buildItemCard(context, items[index]);
+        },
+      ),
+    );
+  }
+
+Widget _buildItemCard(BuildContext context, Item item) {
+  double cardWidth = (fluidWidth(context, 100) - 4 * fluidWidth(context, 1)) / 3; // Calculate card width for 3 cards per row
+
+  return Card(
+    margin: EdgeInsets.symmetric(
+      horizontal: fluidWidth(context, 1),
+      vertical: fluidHeight(context, 2),
+    ),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15),
+      side: BorderSide(
+        color: item.borderColor,
+      ),
+    ),
+    child: SizedBox(
+      width: cardWidth, // Set the card width
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 10,
+            ),
+            child: Text(
+              item.name,
+              textAlign: TextAlign.center, // Center the text
+            ),
+          ),
+          Image.asset(
+            item.imagePath,
+            width: cardWidth - 20, // Adjust image width
+            height: cardWidth - 20, // Maintain aspect ratio
+            fit: BoxFit.cover, // Cover the available space
+          ),
+          IconButton(
+            onPressed: () {
+              _removeItem(item);
+            },
+            icon: const Icon(
+              Icons.delete,
+              color: Colors.red,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+  void _addItem(Item item) {
+    setState(() {
+      items.add(item);
+    });
+  }
+
+  void _removeItem(Item item) {
+    setState(() {
+      items.remove(item);
+    });
+  }
+
+  Widget _buildSendRequestButton(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: 40,
+        width: 230,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
+          child: const Text('Send request'),
+        ),
+      ),
+    );
+  }
+}
+
+class Item {
+  final String name;
+  final String imagePath;
+  final Color borderColor;
+
+  Item({required this.name, required this.imagePath, required this.borderColor});
 }
