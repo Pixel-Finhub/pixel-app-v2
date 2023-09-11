4bcff75fe57578330ef384722f460dddcaa8941c
PatrickMamsery <pkrobert1612@gmail.com>
2023-09-11 10:47:53 +0300

    chore: comment unused packages

--- lib/app/ui/insurance/insurance_details.dart
+++ lib/app/ui/insurance/insurance_details.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:pixel_insurance_v2/app/ui/shared/custom_nav.dart';
-import 'package:pixel_insurance_v2/app/ui/theme/app_colors.dart';
 import 'package:pixel_insurance_v2/app/ui/utils/dimensions.dart';
+// import 'package:pixel_insurance_v2/app/ui/theme/app_colors.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class InsuranceCoverage {
   final String title;
