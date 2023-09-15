125cf3d313a69a66aceb3a11265c3250d699639d
PatrickMamsery <pkrobert1612@gmail.com>
2023-09-15 08:45:08 +0300

    wip: beautified payment_success page a bit

--- lib/app/ui/auth/payment/payment_success.dart
+++ lib/app/ui/auth/payment/payment_success.dart
@@ -8,43 +8,58 @@ class SucceessPayment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-              height: 100,
-              width: 100,
-              child: Image.asset('assets/images/success_image.png')),
-          const Text(
-            'Success !',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.check_circle_outline,
+              color: Colors.green,
+              size: 120.0,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-            child: const Text(
-              'Payment Accepted !!',
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              'Success!',
               style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black26),
+                color: Colors.green,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 90),
-            height: 40,
-            width: 200,
-            child: ElevatedButton(
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              'Your payment was successful.',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 16.0,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
               onPressed: () {
                 Get.to(() => const MyInsurance());
               },
-              child: const Text('My Insurance'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              child: const Text(
+                'View My Insurance',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
